9e4f2fe67c6d70be91e3dcc87460893cdbb161ac
Konstantin Antipochkin <konstantin_a@optimove.com>
2022-06-06 16:16:40 +0300

    Added the Android native part of the public API's

--- android/src/main/java/com/example/optimove_flutter_sdk/OptimoveFlutterSdkPlugin.java
+++ android/src/main/java/com/example/optimove_flutter_sdk/OptimoveFlutterSdkPlugin.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 
 import com.optimove.android.Optimove;
 
+import java.util.Map;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -29,13 +31,79 @@ public class OptimoveFlutterSdkPlugin implements FlutterPlugin, MethodCallHandle
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     switch (call.method){//preparing for alot of different options
+      case "registerUser":
+        handleRegisterUser(call, result);
+        break;
       case "setUserId":
-        String userId = call.argument("userId");
-        Optimove.getInstance().setUserId(userId);
-        result.success(null);
+        handleSetUserId(call, result);
+        break;
+      case "setUserEmail":
+        handleSetUserEmail(call, result);
+        break;
+      case "getVisitorId":
+        handleGetVisitorId(result);
+        break;
+      case "reportEvent":
+        handleReportEvent(call, result);
+        break;
+      case "reportScreenVisit":
+        handleReportScreenVisit(call, result);
+        break;
+      case "getCurrentUserIdentifier":
+        handleGetCurrentUserIdentifier(result);
         break;
       default: result.notImplemented();
     }
+  }
+
+  private void handleGetCurrentUserIdentifier(Result result) {
+    String userIdentifier = Optimove.getInstance().getCurrentUserIdentifier();
+    result.success(userIdentifier);
+  }
+
+  private void handleReportScreenVisit(MethodCall call, Result result) {
+    String screenCategory = call.argument("screenCategory");
+
+    if (screenCategory == null) {
+      Optimove.getInstance().reportScreenVisit(call.argument("screenName"));
+    } else {
+      Optimove.getInstance().reportEvent(call.argument("event"), screenCategory);
+    }
+    result.success(null);
+  }
+
+  private void handleReportEvent(MethodCall call, Result result) {
+    Map<String, Object> parameters = call.argument("parameters");
+
+    if (parameters == null) {
+      Optimove.getInstance().reportEvent(call.argument("event"));
+    } else {
+      Optimove.getInstance().reportEvent(call.argument("event"), parameters);
+
+    }
+    result.success(null);
+  }
+
+  private void handleGetVisitorId(Result result) {
+    String visitorId = Optimove.getInstance().getVisitorId();
+    result.success(visitorId);
+  }
+
+  private void handleSetUserEmail(MethodCall call, Result result) {
+    Optimove.getInstance().setUserEmail(call.argument("email"));
+    result.success(null);
+  }
+
+  private void handleSetUserId(MethodCall call, Result result) {
+    Optimove.getInstance().setUserId(call.argument("userId"));
+    result.success(null);
+  }
+
+  private void handleRegisterUser(MethodCall call, Result result) {
+    String userId = call.argument("userId");
+    String email = call.argument("email");
+    Optimove.getInstance().registerUser(userId, email);
+    result.success(null);
   }
 
   @Override
