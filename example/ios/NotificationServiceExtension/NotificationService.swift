722d36dc71d2998cbfb696ed312a8a3d25dffe1c
Konstantin Antipochkin <konstantin_a@optimove.com>
2022-08-09 15:46:51 -0400

    Changed the NSE from kumulos to Optimove

--- example/ios/NotificationServiceExtension/NotificationService.swift
+++ example/ios/NotificationServiceExtension/NotificationService.swift
@@ -1,8 +1,8 @@
 import UserNotifications
-import KumulosSDKExtension
+import OptimoveNotificationServiceExtension
 
 class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        KumulosNotificationService.didReceive(request, withContentHandler: contentHandler)
+        OptimoveNotificationService.didReceive(request, withContentHandler: contentHandler)
     }
 }
